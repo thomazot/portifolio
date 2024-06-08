acdd5dbcf91f9c02c42eb849b8c9298fb20065f6
Thomaz Toyama <contato@thomazot.com.br>
2024-06-08 09:53:52 -0300

    feat: TextField

--- src/commons/view/index.tsx
+++ src/commons/view/index.tsx
@@ -1,11 +1,44 @@
+import { Property } from 'csstype'
 import React, { ComponentProps } from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-const SView = styled.div``
+type SViewType = {
+  position?: Property.Position
+  top?: Property.Top
+  left?: Property.Left
+  right?: Property.Right
+  bottom?: Property.Bottom
+}
+
+const SView = styled.div<SViewType>`
+  ${({ position, top, bottom, left, right }) => css`
+    ${position &&
+    css`
+      position: ${position};
+    `}
+    ${top &&
+    css`
+      top: ${top};
+    `}
+    ${bottom &&
+    css`
+      bottom: ${bottom};
+    `}
+    ${left &&
+    css`
+      left: ${left};
+    `}
+    ${right &&
+    css`
+      right: ${right};
+    `}
+  `}
+`
 
 type ViewProps = {
   children?: React.ReactNode
-} & ComponentProps<'div'>
+} & ComponentProps<'div'> &
+  SViewType
 
 export const View = ({ children, ...props }: ViewProps) => (
   <SView {...props}>{children}</SView>
--- src/commons/view/view.stories.tsx
+++ src/commons/view/view.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react'
 
+import { Text } from '../text'
+import { Title } from '../title'
 import { View } from '.'
 
 const meta: Meta<typeof View> = {
@@ -13,8 +15,20 @@ type Story = StoryObj<typeof View>
 
 export const Default: Story = {
   args: {
-    children:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis ac arcu sit amet aliquam. Suspendisse ullamcorper fermentum justo, porttitor porttitor elit malesuada at. Nam hendrerit in lectus in iaculis. Phasellus tristique nibh mauris, eget dictum tellus ultricies in. Mauris fringilla consequat lacinia. Vestibulum efficitur quis arcu quis dapibus. Aenean rhoncus est ac urna pharetra pulvinar. Maecenas condimentum massa eu euismod pulvinar. Sed et rhoncus elit. Nam aliquam viverra elit at aliquet. Fusce sit amet tellus auctor, elementum nisl vitae, scelerisque tortor. Suspendisse at est ultricies dui vulputate venenatis eu sed velit.'
+    children: (
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+        mattis ac arcu sit amet aliquam. Suspendisse ullamcorper fermentum
+        justo, porttitor porttitor elit malesuada at. Nam hendrerit in lectus in
+        iaculis. Phasellus tristique nibh mauris, eget dictum tellus ultricies
+        in. Mauris fringilla consequat lacinia. Vestibulum efficitur quis arcu
+        quis dapibus. Aenean rhoncus est ac urna pharetra pulvinar. Maecenas
+        condimentum massa eu euismod pulvinar. Sed et rhoncus elit. Nam aliquam
+        viverra elit at aliquet. Fusce sit amet tellus auctor, elementum nisl
+        vitae, scelerisque tortor. Suspendisse at est ultricies dui vulputate
+        venenatis eu sed velit.
+      </Text>
+    )
   }
 }
 
@@ -23,8 +37,8 @@ export const Multiple: Story = {
   args: {
     children: (
       <>
-        <h1>Title</h1>
-        <h2>Subtitle</h2>
+        <Title>Title</Title>
+        <Title size="subtitle">Subtitle</Title>
       </>
     )
   }
